5daf48206b0cb247d674e2a2b7590ebe47ec2658
LeanoraVilte <tina.ww.lea@gmail.com>
2022-06-27 02:05:15 +0300

    con

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*;
+
+public class Main {
+    public static <string> void main(String[] args) {
+        String a = "Hello ";
+        String b = "New ";
+        String c = "World!";
+
+        System.out.println(a + b + c);
+    }
+}
+
+
