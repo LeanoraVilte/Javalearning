344ca0cec862719b7e3e13328be7fd94f1529a2c
LeanoraVilte <tina.ww.lea@gmail.com>
2022-06-27 09:06:26 +0300

    con3 (comments is added)

--- src/Main.java
+++ src/Main.java
@@ -6,23 +6,23 @@ public class Main {
         String b = "New ";
         String c = "World!";
 
-        System.out.println(a + b + c);
+        System.out.println(a + b + c);//конкантенация строк
 
         int d, f, e;
         d = 1;
         f = 3;
         e = d + f;
-        System.out.println(e);
+        System.out.println(e);//математика: суммирование
 
         d = 6;
         f = 4;
         int w = d*f;
-        System.out.println(w);
+        System.out.println(w);//математика: умножение
 
         a = "your name " +
                 " ";
         d = 42;
-        System.out.println(a + d);
+        System.out.println(a + d);//контакнтенация: строка + число
     }
 }
 
